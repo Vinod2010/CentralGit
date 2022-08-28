6ff4bdd676a7958eb0448cdbef90aa7239a5fe67
Vinod2010 <52598154+Vinod2010@users.noreply.github.com>
2022-08-28 13:33:37 +0530

    2nd commit from vinod trunk1

--- src/main/java/com/itachi/springmvc/dao/AlienDao.java
+++ src/main/java/com/itachi/springmvc/dao/AlienDao.java
@@ -19,7 +19,7 @@ public class AlienDao
 	{
 		Session session = sessionFactory.getCurrentSession();
 		List<Alien> aliens = session.createQuery("from Alien", Alien.class).list();
-		System.out.println("asai hi");
+		System.out.println("asai hi kiya");
 		return aliens;
 	}
 
